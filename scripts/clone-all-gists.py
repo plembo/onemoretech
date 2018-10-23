c374f7314f4bd630a964d2511af4696cdc64cc16
Phil Lembo <phil.lembo@outlook.com>
2018-10-23 14:20:48 -0400

    Update for python 3

--- scripts/clone-all-gists.py
+++ scripts/clone-all-gists.py
@@ -1,35 +1,40 @@
-# Download all my github gists into pwd.
-# Code from https://stackoverflow.com/questions/6724490/pull-all-gists-from-github.
+#!/usr/bin/env python3
+""" Download all my github gists into pwd.
+Code by Fedir RYKHTIK and saranicole, on
+https://stackoverflow.com/questions/6724490/pull-all-gists-from-github.
+Syntax updated to Python 3.
+"""
 
 import json
-import urllib
+# import urllib
 from subprocess import call
-from urllib import urlopen
+from urllib.request import urlopen
 import os
 import math
 # USER = os.environ['USER']
 USER = 'plembo'
 
-perpage=30.0
+perpage = 30.0
 userurl = urlopen('https://api.github.com/users/' + USER)
 public_gists = json.load(userurl)
 gistcount = public_gists['public_gists']
-print "Found gists : " + str(gistcount)
+print("Found gists : " + str(gistcount))
 pages = int(math.ceil(float(gistcount)/perpage))
-print "Found pages : " + str(pages)
+print("Found pages : " + str(pages))
 
-f=open('./contents.txt', 'w+')
+f = open('./contents.txt', 'w+')
 
 for page in range(pages):
     pageNumber = str(page + 1)
-    print "Processing page number " + pageNumber
-    pageUrl = 'https://api.github.com/users/' + USER  + '/gists?page=' + pageNumber + '&per_page=' + str(int(perpage))
-    u = urlopen (pageUrl)
+    print("Processing page number " + pageNumber)
+    pageUrl = 'https://api.github.com/users/' + USER  + '/gists?page=' \
+    + pageNumber + '&per_page=' + str(int(perpage))
+    u = urlopen(pageUrl)
     gists = json.load(u)
     startd = os.getcwd()
     for gist in gists:
         gistd = gist['id']
-        gistUrl = 'git://gist.github.com/' + gistd + '.git' 
+        gistUrl = 'git://gist.github.com/' + gistd + '.git'
         if os.path.isdir(gistd):
             os.chdir(gistd)
             call(['git', 'pull', gistUrl])
