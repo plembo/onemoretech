9331ade38e96e356d9b16b9ac047a066c5da4494
Phil Lembo <phil.lembo@outlook.com>
2019-03-31 15:59:54 -0400

    Refine argparse section; increase paging to 50; Fixed code so it now writes to contents.txt file;  added id and description fields.

--- scripts/clone-users-gists.py
+++ scripts/clone-users-gists.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
-""" Download all my github gists into pwd.
+""" Download all a user's github gists into pwd.
 Code by Fedir RYKHTIK and saranicole, on
 https://stackoverflow.com/questions/6724490/pull-all-gists-from-github.
-Syntax updated to Python 3.
+Syntax updated to Python 3; take username from command line;
 """
 
 import json
-# import urllib
 from subprocess import call
 from urllib.request import urlopen
 import os
 import math
-# USER = os.environ['USER']
-USER = 'plembo'
+import argparse
 
-perpage = 30.0
-userurl = urlopen('https://api.github.com/users/' + USER)
+parser = argparse.ArgumentParser()
+parser.add_argument("user", help="Github user name")
+args = parser.parse_args()
+if args.user:
+    gituser = args.user
+
+perpage = 10.0
+userurl = urlopen('https://api.github.com/users/' + gituser)
 public_gists = json.load(userurl)
 gistcount = public_gists['public_gists']
 print("Found gists : " + str(gistcount))
 pages = int(math.ceil(float(gistcount)/perpage))
 print("Found pages : " + str(pages))
 
-f = open('./contents.txt', 'w+')
+f = open('./contents.txt', 'w')
 
 for page in range(pages):
     pageNumber = str(page + 1)
     print("Processing page number " + pageNumber)
-    pageUrl = 'https://api.github.com/users/' + USER  + '/gists?page=' \
+    pageUrl = 'https://api.github.com/users/' + gituser  + '/gists?page=' \
     + pageNumber + '&per_page=' + str(int(perpage))
     u = urlopen(pageUrl)
     gists = json.load(u)
     startd = os.getcwd()
     for gist in gists:
-        gistd = gist['id']
-        gistUrl = 'git://gist.github.com/' + gistd + '.git'
-        if os.path.isdir(gistd):
-            os.chdir(gistd)
+        gistid = gist['id']
+        gistdesc = gist['description']
+        f.write(str(gistid) + ' ' + str(gistdesc) + '\n')
+        gistUrl = 'git://gist.github.com/' + gistid + '.git'
+        if os.path.isdir(gistid):
+            os.chdir(gistid)
             call(['git', 'pull', gistUrl])
             os.chdir(startd)
+
         else:
             call(['git', 'clone', gistUrl])
+
+f.close()
+
